ca69a70a9bdeb80d274f882cca2ef22dac22f680
jordywolk@outlook.com <0eh4QEXh8Nia>
2020-07-08 14:11:29 +0200

    Update ServerConnection.php
    curl -> GuzzleHTTP

--- src/ServerConnection.php
+++ src/ServerConnection.php
@@ -8,6 +8,7 @@
 
 namespace Jordywolk\Ts3api;
 
+use GuzzleHttp\Exception\TransferException;
 
 class ServerConnection
 {
@@ -18,29 +19,44 @@ class ServerConnection
 
     public function __construct()
     {
-        $this->connection = curl_init();
-        //Pass API key
-        curl_setopt($this->connection, CURLOPT_HTTPHEADER, array('x-api-key:' . config('ts3api.api_key')));
+        $this->connection = new \GuzzleHttp\Client(['base_uri' => config('ts3api.server_ip') . ":" . config('ts3api.server_port')]);
 
-        //Keep response result
-        curl_setopt($this->connection, CURLOPT_RETURNTRANSFER, 1);
+        try {
+            $this->connection->request('GET', "/" . config('ts3api.server_id') . "/clientupdate?client_nickname=" . config('ts3api.nickname') ,[
+                'headers' => [
+                    'x-api-key' => config('ts3api.api_key')
+                ]
+            ]);
+        }
+        catch (TransferException $e) {
+            report($e);
+        }
 
-        //Set nickname to value specified in config file
-        $url = config('ts3api.server_ip') . ":" . config('ts3api.server_port') . "/" . config('ts3api.server_id') . "/clientupdate?client_nickname=" . config('ts3api.nickname');
-        curl_setopt($this->connection, CURLOPT_URL, $url);
-        curl_exec($this->connection);
+//TODO: Remove
+
+//        $this->connection = curl_init();
+//        //Pass API key
+//        curl_setopt($this->connection, CURLOPT_HTTPHEADER, array('x-api-key:' . config('ts3api.api_key')));
+//
+//        //Keep response result
+//        curl_setopt($this->connection, CURLOPT_RETURNTRANSFER, 1);
+//
+//        //Set nickname to value specified in config file
+//        $url = config('ts3api.server_ip') . ":" . config('ts3api.server_port') . "/" . config('ts3api.server_id') . "/clientupdate?client_nickname=" . config('ts3api.nickname');
+//        curl_setopt($this->connection, CURLOPT_URL, $url);
+//        curl_exec($this->connection);
     }
 
     public function __destruct()
     {
-        curl_close($this->connection);
+
     }
 
     //TODO: Allow '-xxx' flags
     protected function send_command(string $command, array $parameters = null)
     {
         //Base URL
-        $url = config('ts3api.server_ip') . ":" . config('ts3api.server_port') . "/" . config('ts3api.server_id') . "/" . $command;
+        $url = "/" . config('ts3api.server_id') . "/" . $command;
 
         //Add parameters to URL, if given any
         if($parameters != null) {
@@ -52,10 +68,17 @@ class ServerConnection
             $url = rtrim($url, "&");
         }
 
-        //Execute command on server
-        curl_setopt($this->connection, CURLOPT_URL, $url);
-        $result = curl_exec($this->connection);
+        try {
+            $response = $this->connection->request('GET', $url ,[
+                'headers' => [
+                    'x-api-key' => config('ts3api.api_key')
+                ]
+            ]);
+        }
+        catch (TransferException $e) {
+            report($e);
+        }
 
-        return json_decode($result);
+        return json_decode($response);
     }
 }
